66b6e9dff7c24be41251a544cfbd6983794365e7
Azie Melza Pratama <aziemp66@gmail.com>
2022-03-18 19:37:50 +0700

    Validating User

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,14 @@
 const User = require("../model/user.model");
+const validation = require("../util/validation");
 
 async function register(req, res) {
+	//Validating data
+	const { error } = validation.registerValidation(req.body);
+
+	if (error) {
+		return res.status(400).send(error.details[0].message);
+	}
+
 	const user = new User({
 		name: req.body.name,
 		email: req.body.email,
